d4e6e68c725be9ccb6990ec59b4c5dd3983326e7
Medic-bag <rileygirling@gmail.com>
2024-12-02 11:02:55 -0600

    made 2 variables

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
-// Project Title
-// Your Name
-// Date
+// Swagalicous Tower defense
+// Riley Sane and Chase Buniak
+// January whenever, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+// Variables and Constants
+let grid;
+let cellSize = 20;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
