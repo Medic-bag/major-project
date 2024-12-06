99ce0e13320992abc36f818516b827341cb65d87
Sane3702068 <rileygirling@gmail.com>
2024-12-06 11:04:18 -0600

    Continued work on enemy classes.
    Did more work on the movement portion of the enemy classes, tried to do a bug fix, made a to do list.

--- sketch.js
+++ sketch.js
@@ -5,6 +5,10 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// to do list
+// - floor the lines for the path
+// - get rid of console.log in enemy.move()
+
 // Classes
 class Tower {
   constructor(x, y, radius) {
@@ -30,11 +34,13 @@ class Sniper extends Tower {
 
 }
 
+// Parent Class for the enemies.
 class Enemy {
-  constructor(x, y, radius) {
+  constructor(y) {
     this.x = 0;
     this.y = y;
-    this.radius = radius;
+    this.radius = 10;
+    this.speed = 1;
   }
 
   display() {
@@ -42,23 +48,45 @@ class Enemy {
     circle(this.x, this.y, this.radius);
   }
 
-  move()  {
-
+  move() {
+    if (this.x <= width/10 - this.speed) {
+      this.x += this.speed;
+      console.log(this.x);
+    }
+    if (this.y >= height/1.5 && this.x === width/10) {
+      this.y += this.speed;
+    }
+    if (this.x <= width / 5 && this.y === height/1.5) {
+      this.x += this.speed;
+    }
+    if (this.y <= height/3 && this.x === width/5) {
+      this.y -= this.speed;
+    }
   }
 }
 
+// The Class for our 'normal' enemies; enemies with medium damage, speed and health to set a baseline for the other enemy types.
 class NormalEnemy extends Enemy {
-  constructor(health, speed, damage) {
-    super(x, y, radius);
-    this.health = health;
-    this.speed = speed;
-    this.damage = damage;
+  constructor(y) {
+    super(y);
+    this.health = 10;
+    this.speed += 1;
+    this.damage = 1;
     this.color = 'red';
   }
+  display() {
+    fill(this.color);
+    super.display();
+  }
+  
+  move() {
+    super.move();
+  }
+
 }
 
 // Variables and Constants
-
+let grid = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -66,11 +94,16 @@ function setup() {
 
 function draw() {
   background(220);
-  drawPath();
+  for (let enemy of grid) {
+    enemy.move();
+    enemy.display();
+    drawPath();
+    
+  }
 }
 
 function drawPath() {
-  line(0, height/2,  width/ 10, height/2);
+  line(0, height/2,  width/10, height/2);
   line(width/10, height/2, width/10, height/1.5);
   line(width/10, height/1.5, width/5, height/1.5);
   line(width/5, height/1.5, width/5, height/3);
@@ -88,3 +121,9 @@ function drawPath() {
   line(width/10 * 9, height/1.5, width/10 * 9, height/2);
   line(width/10 * 9, height/2, width, height/2);
 }
+
+function mousePressed() {
+  let someEnemy = new NormalEnemy(height/2);
+  grid.push(someEnemy);
+}
+
